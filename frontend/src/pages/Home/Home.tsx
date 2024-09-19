6c7464e1c9489e9ddf764a7288e63919df8a6b09
daniel <daniellimafreita@gmail.com>
2024-09-18 22:53:43 -0300

    Conf: Configurando eslint e prettier

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HomePageStyle } from './HomeStyles';
+
+const Home = () => {
+  return <HomePageStyle></HomePageStyle>;
+};
+
+export default Home;
---
+++ frontend/src/pages/Home/HomeStyles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { colors } from '../../styles/Theme';
+
+export const HomePageStyle = styled.main`
+  background-image: ${colors.gradientDarkGray};
+  display: flex;
+  height: 100vh;
+`;
