b074577e3eaebd6dd1f9530efcdcc1f172ca406d
daniel <daniellimafreita@gmail.com>
2024-10-01 13:21:44 -0300

    :sparkles:Feat: Corrigindo scrollToSection para o componente de sectionRefAboutUs

--- frontend/src/containers/Home/SidebarHome/SidebarHome.tsx
+++ frontend/src/containers/Home/SidebarHome/SidebarHome.tsx
@@ -8,9 +8,36 @@ interface SidebarHomeProps {
 }
 
 const SidebarHome: React.FC<SidebarHomeProps> = ({ sectionRefAboutUs, sectionRefPlans, sectionRefContacts }) => {
+
+  const scrollAmout: number = 0;
+  const scrollStep: number = -10;
+  let currentScroll: number = 100;
+
+  const smoothScroll = () => {
+    if(currentScroll > scrollAmout){
+      window.scrollBy(0, scrollStep);
+      currentScroll += scrollStep;
+      requestAnimationFrame(smoothScroll);
+    }else{
+      currentScroll = 100;
+    }
+  }
+
   const scrollToSection = (sectionRef: React.RefObject<HTMLDivElement>) => {
     if (sectionRef.current) {
-      sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+      sectionRef.current.scrollIntoView(
+        { 
+          behavior: 'smooth',
+          block: 'start'
+        }
+      );
+
+      if(sectionRef === sectionRefAboutUs){
+        setTimeout(() => {
+          smoothScroll();
+        }, 500)
+      }
+      
     }
   };
   
