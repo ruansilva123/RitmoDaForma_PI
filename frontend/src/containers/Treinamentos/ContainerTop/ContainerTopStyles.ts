463705e23c46bbd33d64a3b1b4b6f612c01bd80b
Levi <levi.tobio2505@gmail.com>
2024-09-30 21:56:30 -0300

    Feat: Adicionando rotas à página de treinamento e iniciando a estilização.

---
+++ frontend/src/containers/Treinamentos/ContainerTop/ContainerTopStyles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { colors } from "../../../styles/Theme";
+
+
+export const ContainerTop = styled.div`
+
+`
+
+export const ImageWaveWhite = styled.img`
+    position: absolute;
+    bottom: 62vh;
+    right: 0vh;
+    height: 40vh;
+
+`
+
+export const BackgroundContainerTop = styled.div`
+    background-color: white;
+    position: absolute;
+    bottom: 30vh;
+    height: 32vh;
+    width: 100vw;
+
+
+`
