e889e57308393928f2b638c41f420f85b280949b
daniel <daniellimafreita@gmail.com>
2024-10-10 19:00:52 -0300

    feat: desenvolvendo o context api para o modal de treinamentos

--- frontend/src/components/Treinamentos/Modal/Modal.tsx
+++ frontend/src/components/Treinamentos/Modal/Modal.tsx
@@ -3,23 +3,25 @@ import * as S from "./ModalStyles";
 
 
 interface ModalProps {
-    openModal: () => void;
+    closeModal: () => void;
+    content: {
+        video: string, 
+        title: string,
+        desc: string
+    } | null,
 }
 
-const Modal: React.FC<ModalProps> = ({openModal}) =>{
+const Modal: React.FC<ModalProps> = ({closeModal, content}) =>{
     return(
-        <S.BackgroundBlur onClick={() => openModal()} >
+        <S.BackgroundBlur onClick={() => closeModal()} >
             <S.ModalContainer>
                 <S.CloseIcon/>
                 <S.Video></S.Video>
                 <S.ContentModal>
-                    <S.TitleModal>Halteres</S.TitleModal>
+                    <S.TitleModal>{content?.title}</S.TitleModal>
                     <S.RedLine/>
-                    <S.DescriptionModal>Lorem ipsum dolor sit amet, consect.  dolor sit amet, consecdolor. 
-                        Lorem ipsum dolor sit amet, consect.  dolor sit amet, consecdolor. 
-                        Lorem ipsum dolor sit amet, consect.  dolor sit amet, consecdolor. 
-                        Lorem ipsum dolor sit amet, consect.  dolor sit amet, consecdolor. 
-                        Lorem ipsum dolor sit amet, consect.  dolor sit amet, consecdolor.
+                    <S.DescriptionModal>
+                        {content?.desc}
                     </S.DescriptionModal>
                 </S.ContentModal>
             </S.ModalContainer>
