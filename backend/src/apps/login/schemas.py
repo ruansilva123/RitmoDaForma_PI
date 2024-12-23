e2285838098ef2b9cec494f90ce14680c83ecb88
ritmodaforma <ritmodaforma19@gmail.com>
2024-12-23 17:09:53 -0300

    :fire: remove: Removendo diretório backend

--- backend/src/apps/login/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class LoginSchema(BaseModel):
-
-    email : str
-    password : str
--- backend/src/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from src.core.routers import core_routers
-
-
-app = FastAPI()
-
-
-app.include_router(core_routers, prefix='/api', tags=['all_urls'])
